d519f6d05f4b526e0a054d0702443e5d0379ebc9
mauricio <mauricinhosantanna@hotmail.com>
2026-08-05 21:43:32 -0300

    novos exercicios

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ import express from 'express' //type module
 //inicializando o express -new
 const app = express()
 
-app.get('/api/somar', (req, res) => {
-    const  num1 = Number (req.query.num1)
-    const  num2 = Number (req.query.num2)
-    res.send({message:num1 + num2})})
+app.get('/api/salario', (req, res) => {
+    const  valorhora = Number (req.query.valorhora)
+    const  horasTrabalhadas = Number (req.query.horasTrabalhadas)
+    const salario = valorhora * horasTrabalhadas
+    res.send({message:salario})})
+
+
+app.listen(3000, () => {
+ console.log('Server is running on http://localhost:3000')
+})
+
+
+app.get('/api/peso', (req, res) => {
+    const  peso1 = Number (req.query.peso1)
+    const  peso2 = Number (req.query.peso2)
+    const  peso3 = Number (req.query.peso3)
+    const  peso4 = Number (req.query.peso4)
+    const  peso5 = Number (req.query.peso5)
+    const media = (peso1 + peso2 + peso3 + peso4 + peso5) / 5
+    res.send({message:media})})
 
 
 app.listen(3000, () => {
